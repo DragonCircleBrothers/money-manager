a48dee2803a9b379d932ef17338cdc856df69f97
lhhyung91 <lhhyung91@gmail.com>
2021-02-22 20:55:11 +0900

    feat: Styling daily consumed list Modal

--- client/src/ts/main.ts
+++ client/src/ts/main.ts
@@ -1,3 +1,7 @@
 import myDoughnutChart from "./chart/chart_render";
+import detailPopup from "./detail/detail_popup";
+import addDetaillist from "./add-detail-list/add-detail-list";
 
 myDoughnutChart();
+detailPopup();
+addDetaillist();
