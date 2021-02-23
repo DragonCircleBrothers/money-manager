fefea2cb2a565e5c5b943148d1341d266bd35087
jiwon <iamkjw77@naver.com>
2021-02-23 20:50:22 +0900

    feat: Modify modal

--- client/src/ts/modal/addModal.ts
+++ client/src/ts/modal/addModal.ts
@@ -5,6 +5,7 @@ const addModal = (() => {
   const $modal = document.querySelector(".modal") as HTMLElement;
   const $addModal = document.querySelector(".add-modal") as HTMLElement;
   const $incomeModal = document.querySelector(".income-modal") as HTMLElement;
+  const $outcomeModal = document.querySelector(".outcome-modal") as HTMLElement;
   const $paymentModal = document.querySelector(".payment-modal") as HTMLElement;
 
   const close = () => {
@@ -16,9 +17,25 @@ const addModal = (() => {
     $addModal.style.display = "none";
     $incomeModal.style.display = "block";
 
-    const $price = document.querySelector(".price__original") as HTMLElement;
-    $price.textContent = price + "";
+    const $incomePrice = document.querySelector(
+      "income-modal__price > .price__original"
+    ) as HTMLElement;
+    console.log($incomePrice);
+
+    // $incomePrice.textContent = price + "";
   };
+
+  const outcomeModalRender = (price: number) => {
+    $addModal.style.display = "none";
+    $outcomeModal.style.display = "block";
+
+    const $outcomePrice = document.querySelector(
+      ".outcome-modal__price > .price__original"
+    ) as HTMLElement;
+    $outcomePrice.textContent = price + "";
+  };
+
+  // const paymentModalRedner = () => {};
 
   return {
     addModalRender(date: string) {
@@ -28,14 +45,18 @@ const addModal = (() => {
       const $date = document.querySelector(".add-modal__date") as HTMLElement;
       $date.textContent = date;
     },
-    // outcomeModalRender() {},
-    // paymentModalRedner() {},
     eventHandler() {
       $addModal.onclick = (e: MouseEvent) => {
         const target = e.target as HTMLElement;
         if (target.classList.contains("add-modal__closed")) close();
         if (target.classList.contains("btn__income")) incomeModalRender(2000);
-        // if (target.classList.contains("btn__outcome"))
+        if (target.classList.contains("btn__outcome")) outcomeModalRender(2000);
+      };
+
+      $incomeModal.onclick = (e: MouseEvent) => {
+        const target = e.target as HTMLElement;
+        const $category = document.querySelector(".income-modal__category");
+        if (target === $category) console.log("ddd");
       };
     },
   };
