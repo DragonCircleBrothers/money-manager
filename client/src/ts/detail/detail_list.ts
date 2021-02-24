196a0cdad87808f4d49b7cc4ad17a397ecc37367
lhhyung91 <lhhyung91@gmail.com>
2021-02-24 16:00:28 +0900

    feat: Develop detail list function..

--- client/src/ts/detail/detail_list.ts
+++ client/src/ts/detail/detail_list.ts
@@ -1,60 +1,54 @@
 import billModal from "../modal/billModal";
 
 const $main = document.querySelector(".main") as HTMLElement;
-// const $consumptionDetailList = document.querySelector(
-//   ".consumption-detail-list"
-// ) as HTMLElement;
-const currentDate = new Date();
 
 const renderDetailList = () => {
   const $consumptionCont = document.createElement("div");
+  // const $consumptionDetailList = document.createElement("ul");
+
+  const $calendarContainer = document.querySelector(
+    ".calendar-container"
+  ) as HTMLElement;
 
   $consumptionCont.classList.add("consumption-container");
 
-  $consumptionCont.innerHTML = `
-  <ul class="consumption-detail-list">
-        <li>
-          <label for="categorybadge">
-            <div> 
-            <input id="categorybadge" class="fas fa-utensils fa-2x" type="radio" disabled>
-            <span class="">교통비</span>
-            </div>
-            <span class="">저녁</span>
-            <span>25000원</span>
-          </label>
-        </li>
-        <li>
-          <label for="categorybadge">
-            <div> 
-            <input id="categorybadge" class="fas fa-utensils fa-2x" type="radio" disabled>
-            <span class="">식사</span>
-            </div>
-            <span class="">점심</span>
-            <span>25000원</span>
-          </label>
-        </li>
-        <li>
-          <label for="categorybadge">
-            <div> 
-            <input id="categorybadge" class="fas fa-utensils fa-2x" type="radio" disabled>
-            <span class="">식사</span>
-            </div>
-            <span class="">점심</span>
-            <span>25000원</span>
-          </label>
-        </li>
-      </ul>
+  $calendarContainer.addEventListener("click", (e: MouseEvent) => {
+    if ((e.target as HTMLElement).classList.contains("calendar-sell")) {
+      $consumptionCont.innerHTML = `
+    <ul class="consumption-detail__list">
+    <li>
+      <label for="categorybadge">
+        <div>
+        <input id="categorybadge" class="fas fa-utensils fa-2x" type="radio" disabled>
+        <span class="">교통비</span>
+        </div>
+        <span class="">저녁</span>
+        <span>25000원</span>
+      </label>
+    </li>
+    <li>
+      <label for="categorybadge">
+        <div>
+        <input id="categorybadge" class="fas fa-utensils fa-2x" type="radio" disabled>
+        <span class="">교통비</span>
+        </div>
+        <span class="">저녁</span>
+        <span>25000원</span>
+      </label>
+    </li>
+    </ul>
   `;
-  $main.appendChild($consumptionCont);
+    }
+    $main.appendChild($consumptionCont);
+  });
 
-  const $consumptionDetailList = document.querySelector(
-    ".consumption-detail-list"
-  ) as HTMLElement;
+  $consumptionCont.addEventListener("click", (e: MouseEvent) => {
+    if (document.querySelectorAll(".consumption-detail__list > li")) {
+      // console.log((e.target as HTMLElement).dataset);
 
-  $consumptionDetailList.addEventListener("click", (e: MouseEvent) => {
-    console.log(e.target);
-    billModal.billModalRender();
-    billModal.eventHandler();
+      billModal.billModalRender();
+      billModal.eventHandler();
+    }
   });
 };
 
