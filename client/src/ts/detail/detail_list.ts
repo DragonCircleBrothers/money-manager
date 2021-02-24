9eab78ad4cff6d8c5e9699f17249cbce03beb92d
lhhyung91 <lhhyung91@gmail.com>
2021-02-24 20:41:12 +0900

    for merge...

--- client/src/ts/detail/detail_list.ts
+++ client/src/ts/detail/detail_list.ts
@@ -11,11 +11,13 @@ const renderDetailList = () => {
     ".calendar-container"
   ) as HTMLElement;
 
+  const $calendarCell = document.querySelector(".calendar-cell") as HTMLElement;
+
   $consumptionCont.classList.add("consumption-container");
   $consumptionDetailList.classList.add("consumption-detail__list");
 
   $calendarContainer.addEventListener("click", (e: MouseEvent) => {
-    if ((e.target as HTMLElement).classList.contains("calendar-sell")) {
+    if ($calendarCell) {
       $consumptionDetailList.innerHTML = `
     <li>
       <label for="categorybadge">
@@ -34,7 +36,10 @@ const renderDetailList = () => {
   });
 
   $consumptionCont.addEventListener("click", (e: MouseEvent) => {
-    if (document.querySelectorAll(".consumption-detail__list > li")) {
+    const $detailLIst = document.querySelectorAll(
+      ".consumption-detail__list > li"
+    );
+    if ($detailLIst) {
       // const $selected = document.querySelector(".selected") as HTMLElement;
       // const target = e.target as HTMLElement;
       // const selectedDate = target.dataset.date + "";
