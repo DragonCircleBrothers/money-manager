b71e9dd9ab8a6902016a8649d0ee6358307b710c
yhun940731@gmail.com <yhun940731@gmail.com>
2021-02-25 13:52:22 +0900

    income outcome text refresh function when used addModal in ChartMain (solved: #31)

--- client/src/ts/calendar/postAccount.ts
+++ client/src/ts/calendar/postAccount.ts
@@ -1,14 +1,24 @@
 import axios from "axios";
 import AccountItem from "../type";
 import calendarListRender from "./calendarListRender";
+import chartRender from "../chart/chart_render";
 import globalState from "../globalState";
 
 const postAccounts = async (account: AccountItem): Promise<void> => {
   await axios.post("http://localhost:1111/api/account", { ...account });
-  calendarListRender(
-    globalState.currentDate.getFullYear(),
-    globalState.currentDate.getMonth()
-  );
+
+  if (globalState.pageLocation === "chart") {
+    chartRender(
+      globalState.currentDate.toISOString().slice(0, 7),
+      account.type,
+      globalState.currentDate
+    );
+  } else {
+    calendarListRender(
+      globalState.currentDate.getFullYear(),
+      globalState.currentDate.getMonth()
+    );
+  }
 };
 
 export default postAccounts;
